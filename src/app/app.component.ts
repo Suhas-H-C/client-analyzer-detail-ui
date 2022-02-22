2c877d3862dcbde6de78bae11404d2193aed9c29
Suhas H C <suhashc193@gmail.com>
2022-02-22 13:44:26 +0530

    Added intercomponent interaction with @ViewChild()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FeatureDevelopComponent } from './content/feature-develop/feature-develop.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
+
+  /**
+   * To change something in child component accessed we implement AfterViewInit interface
+   * @Override method ngAfterViewInIt()
+   */
+  ngAfterViewInit(): void {
+    //this.featureDevelopComponent.myName="Hi from parent app";
+    console.log(this.featureDevelopComponent.myName)
+  }
   ngOnInit(): void {
   }
   title = 'angular-console';
   
+  /**
+   * Accesing child component
+   */
+  @ViewChild(FeatureDevelopComponent) featureDevelopComponent!:FeatureDevelopComponent;
+
   listOfAnims:Array<string>=['cat','dog','donkey','tiger','lion'];
   yourReferenceTitle:string="feature"
 }
