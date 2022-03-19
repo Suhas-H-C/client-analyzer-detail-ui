0f62dd46df9ccef86c6ddec3f323784795f8a039
Suhas H C <suhashc193@gmail.com>
2022-03-19 17:04:35 +0530

    Added intendation

--- src/app/content/content-service/content-service.service.ts
+++ src/app/content/content-service/content-service.service.ts
@@ -15,7 +15,7 @@ export class ContentServiceService {
   getEmployeeRecords(): Observable<any> {
     return this.appService.getEmployeeRecords();
   }
-  parseCsvSend(file:File):Observable<any>{
+  parseCsvSend(file: File): Observable<any> {
     return this.appService.parseCsvSend(file);
   }
 }
