63bc804d23d3f24e4584258eaedb26ac8e9f2052
Suhas H C <suhashc193@gmail.com>
2023-03-08 11:57:44 +0530

    Docker configs added and updated budling capabilities

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/basic-app'),
+      dir: require('path').join(__dirname, './coverage/ang-dashboard'),
       subdir: '.',
       reporters: [
         { type: 'html' },
